61fc38c658f511d8555bdd8a7ebeaa9045569fc2
Joao Cabral de Matos Carvalho <jcmcgoojcmc@gmail.com>
2025-12-09 13:12:07 +0100

    first init 02-react

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+function Header () {
+  return (
+    <header>
+      <h1>
+        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+          viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <polyline points="16 18 22 12 16 6"></polyline>
+          <polyline points="8 6 2 12 8 18"></polyline>
+        </svg>
+        DevJobs
+      </h1>
+      <nav>
+        <a href="index.html">Inicio</a>
+        <a href="empleos.html">Empleos</a>
+        <a href="react.html">React</a>
+      </nav>
+    </header>
+  )
+}
+
+
+export default Header;  // Exportamos el componente
